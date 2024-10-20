5da9856c4589912c01d7be994886bc4c84a88a34
Daria Kuznetsova <kuzneda2@fel.cvut.cz>
2024-10-20 12:08:32 +0200

    start project

---
+++ TimeKeep/src/test/java/sir/timekeep/TimeKeepApplicationTests.java
@@ -0,0 +1,13 @@
+package sir.timekeep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeKeepApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
