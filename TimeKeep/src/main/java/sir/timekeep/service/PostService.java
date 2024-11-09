641074f862fed288bb8b0ecafe22b159bc9f082e
Matěj Charousek <charoma1@fel.cvut.cz>
2024-11-09 12:19:29 +0100

    Modify PostService

--- TimeKeep/src/main/java/sir/timekeep/service/PostService.java
+++ TimeKeep/src/main/java/sir/timekeep/service/PostService.java
@@ -4,23 +4,24 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import sir.timekeep.dao.PostDao;
-import sir.timekeep.model.Capsule;
-import sir.timekeep.model.Memory;
-import sir.timekeep.model.User;
-import sir.timekeep.model.Group;
-import sir.timekeep.model.Media;
+import sir.timekeep.model.*;
 
 import java.time.LocalDateTime;
 
 import java.util.List;
+import java.util.Objects;
 
 @Service
 public class PostService {
     private final PostDao postDao;
+    private final PostDao memoryDao;
+    private final PostDao capsuleDao;
 
     @Autowired
-    public PostService(PostDao postDao) {
+    public PostService(PostDao postDao, PostDao memoryDao, PostDao capsuleDao) {
         this.postDao = postDao;
+        this.memoryDao = memoryDao;
+        this.capsuleDao = capsuleDao;
     }
 
     @Transactional
@@ -34,4 +35,25 @@ public class PostService {
         Capsule capsulePost = new Capsule(name, media, timeOfCreation, postCreator, group, timeOfOpening);
         postDao.persist(capsulePost);
     }
+
+    // createPost usetri radek kodu, ale na druhou stranu by se mohl zavolat createPost a post by se neulozil
+    // do Memory/Capsule, coz nechceme. Private ale nastavit nejde kvuli Transactional
+    @Transactional
+    protected void createPost(Post post){
+        Objects.requireNonNull(post);
+        postDao.persist(post);
+    }
+
+    @Transactional
+    public void createMemory(Post post){
+        createPost(post);
+        memoryDao.persist(post);
+    }
+
+    @Transactional
+    public void createCapsule(Post post){
+        createPost(post);
+        capsuleDao.persist(post);
+    }
+
 }
