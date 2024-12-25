c997d20e7b7254bea23bd7f06ecacce3cef08300
Daria Kuznetsova <kuzneda2@fel.cvut.cz>
2024-12-25 13:17:11 +0100

    stronger validation in UserService of persist()

--- TimeKeep/src/main/java/sir/timekeep/controller/UserController.java
+++ TimeKeep/src/main/java/sir/timekeep/controller/UserController.java
@@ -1,10 +1,8 @@
 package sir.timekeep.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import sir.timekeep.model.User;
 import sir.timekeep.service.UserService;
@@ -21,14 +19,8 @@ public class UserController {
 
     @PostMapping("/register")
     public ResponseEntity<Void> registerUser(@RequestBody User user) {
-        System.out.println("Received user: " + user);
-        if (user == null) {
-            return ResponseEntity.badRequest().build();
-        }
         userService.persist(user);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
-
-
 
 }
